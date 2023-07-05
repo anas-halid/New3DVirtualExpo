07213041876756303d46a7dd179b768287d474dc
Smartplus50 <132195854+anas-halid@users.noreply.github.com>
2023-07-05 20:41:00 +0800

    Added 3d booth
    added 3d product booth, looks more stylish

--- file/src/components/Shop.js
+++ file/src/components/Shop.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import bg from './booth3.png' ; 
+
 
 export default function Shop() {
   return (
-    <div>  
+    <div style={{backgroundColor: '#4E6E83'}}>  
+       
           <h1>Welcome to our Virtual Product Exhibition Booth!</h1>
     <h3 id="centeralign">Here you can learn about our latest products and services.</h3>
     <ul>
-      <li id ="menu"><a href="">View Product Video</a></li>
-      <li id ="menu"><Link to='/items'>view products</Link></li>
-      <li id ="menu"><a href="">"Live Demo"</a></li>
-      <li id ="menu"><a href="">"Live Chat"</a></li>
-
+      <li id ="menu" style={{ position: 'relative', left: 90, top: 515 }} ><a href="">View Product Video</a></li>
+      <li id ="menuright"style={{ position: 'relative', left: -85, top: 440}}><Link to='/items'>view products</Link></li>
+      <li id ="menu" style={{ position: 'relative', left: 90, top: 515 }}><a href="">"Live Demo"</a></li>
+      <li id ="menuright" style={{ position: 'relative', left: -85, top: 440}}><a href="">"Live Chat"</a></li>
+      <img id ="bground" src={bg} />
 
     </ul>
     </div>
